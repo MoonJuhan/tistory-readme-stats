cafa428bedd67dffbc432e728db29d53339148d9
MoonJuhan <codemike@naver.com>
2021-07-29 18:16:25 +0900

    fix: 태그 텍스트 사이즈 관련 로직 수정

--- src/tags.js
+++ src/tags.js
@@ -1,11 +1,17 @@
 export const refineTags = (tags) => {
   let tagMarkup = '';
   let xPos = 25;
-  console.log(tags);
+  
   tags.forEach((tag) => {
+    let size = 16;
     const korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+    const capitalEnglish = /[A-Z]/;
 
-    const size = tag.length * (korean.test(tag) ? 12 : 9);
+    for (let i = 0; i < tag.length; i++) {
+      const letter = tag[0];
+      size += korean.test(letter) ? 10 : capitalEnglish.test(letter) ? 7.5 : 5;
+    }
+
     tagMarkup += `
     <svg data-testid="lang-items" x="${xPos}" width="${size}" viewBox="0 0 ${size} 19">
         <g style="position: relative;">
